f8fbbbe08947cfcbf73feb1a447931bef63d0d93
CodeChinh <khleson79929@gmail.com>
2020-11-27 16:31:42 +0700

    Cua chinh

--- DoAnASP/DoAnASP/Areas/Admin/Models/DatVeModel.cs
+++ DoAnASP/DoAnASP/Areas/Admin/Models/DatVeModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +11,13 @@ namespace DoAnASP.Areas.Admin.Models
     {
         [Key]
         public int IdUser { get; set; }
+        [StringLength(maximumLength:10,MinimumLength =25,ErrorMessage ="Pleas enter into valid Username")]
+        [Required(ErrorMessage = "Pleas enter into valid Username")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Pleas enter into valid Username")]
         public string Password { get; set; }
+        [StringLength(maximumLength: 10, MinimumLength = 25, ErrorMessage = "Pleas enter into valid Username")]
+        [Required(ErrorMessage = "Pleas enter into valid Username")]
         public string HoTen { get; set; }
         public DateTime NgaySinh { get; set; }
         public bool GioiTinh { get; set; }
