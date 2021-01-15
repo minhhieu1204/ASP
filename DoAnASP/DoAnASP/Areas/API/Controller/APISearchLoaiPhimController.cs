6dcedac3b5d2d778cfa360a59579a5398d8b5f21
LDPhuc07 <Phucle.20000101@gmail.com>
2021-01-15 14:34:24 +0700

    final

---
+++ DoAnASP/DoAnASP/Areas/API/Controller/APISearchLoaiPhimController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DoAnASP.Areas.Admin.Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DoAnASP.Areas.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APISearchLoaiPhimController : ControllerBase
+    {
+        private readonly DPContext _context;
+
+        public APISearchLoaiPhimController(DPContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<APISearchLoaiPhimController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<APISearchLoaiPhimController>/5
+        [HttpGet("GetData")]
+        public string Get(string keyword)
+        {
+                var typeFilm = from s in _context.loaiPhimModels
+                               where s.TenLoaiPhim.Contains(keyword)
+                               select new {
+                                   s.IdLoaiPhim,
+                                   s.TenLoaiPhim
+                               };
+                return JsonConvert.SerializeObject(typeFilm);
+        }
+
+        // POST api/<APISearchLoaiPhimController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<APISearchLoaiPhimController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<APISearchLoaiPhimController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
