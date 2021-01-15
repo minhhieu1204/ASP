eb597c15c080ee020511e55add1587e4a6e5fef1
CodeChinh <khleson79929@gmail.com>
2021-01-15 11:33:21 +0700

    hj

--- DoAnASP/DoAnASP/wwwroot/footer/js/A-Z-List.js
+++
@@ -1,7 +0,0 @@
-let row = document.getElementsByTagName("tr");
-console.log(row);
-for (let i = 0; i < row.length; i++){
-    if(i%2 != 0){
-        row[i].style.backgroundColor="#F5F5F5";
-    }
-}
